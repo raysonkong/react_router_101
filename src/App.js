a635ee83204c52e888e1a47a9cebbac7ef6fbd00
Rayson Kong <kong.rayson@gmail.com>
2021-03-24 17:49:46 +0800

    basic React Router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Link } from 'react-router-dom';
+
+const HomePage = (props) => {
+  console.log(props.location.pathname)
+  return(
+    <div>
+      <h1>HomePage</h1>
+      <Link to='/topics'>Topics List</Link>
+      <button onClick={() => props.history.push('/topics')}>Topics</button>
+    </div>
+  );
+}
+
+const TopicsList = (props) => {
+  console.log(props)
+  const {range} = props;
+  return(
+    <div>
+      <h1>Topic List Page</h1>
+      <Link to={`${props.match.url}/13`}>To Topic 13</Link>
+      <Link to={`${props.match.url}/17`}>To Topic 17</Link>
+      <Link to={`${props.match.url}/21`}>To Topic 21</Link>
+    </div>
+  );
+}
+
+const TopicDetail = (props) => { 
+  console.log(props)
+  return(
+    <div>
+      <h1>Topic Detail: {props.match.params.topicId}</h1>
+
+
+    </div>
+  );
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/abc/topics' component={TopicsList}/>
+      <Route exact path='/def/topics' component={TopicsList}/>
+      <Route path='/topics/:topicId' component={TopicDetail}/>
     </div>
   );
 }
